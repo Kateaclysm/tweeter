e52956347b3f838dedee3edbf069d418201cd121
Katelynn <grayspence2@hotmail.com>
2023-11-15 17:44:40 -0500

    tweet display area and appropriate styling added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,103 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+const createTweetElement = function(tweetData) {
+  // Create the HTML structure using `tweetData`
+
+  /* VV formats the date to something human-readable!*/
+  const timeAgo = moment(tweetData.created_at).fromNow();
+
+  const $tweet = $(`<article class="tweet">
+    <header>
+      <img src="${tweetData.user.avatars}" alt="${tweetData.user.name}'s avatar"/>
+      <h2>${tweetData.user.name}</h2>
+      <span>${tweetData.user.handle}</span>
+    </header>
+    <p>${tweetData.content.text}</p>
+    <footer>
+      <time>${timeAgo}</time>
+    </footer>
+  </article>`);
+
+  return $tweet;
+};
+$(document).ready(function(){
+
+  const tweetData =  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  
+  }
+
+  const $tweet = createTweetElement(tweetData);
+  console.log($tweet);
+
+$('#tweets-container').append($tweet);
+})
+
+
+const renderTweets = function(tweets) {
+  // Empty the container first!
+  $('#tweets-container').empty();
+
+  //Loop through the array of tweets!
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+};
+
+
+
+$(document).ready(function(){
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+  renderTweets(data);
+
+  $('#new-tweet-submission').on("submit", function(event) {
+    event.preventDefault();
+    console.log("inside submit event handler", event.target);
+    console.log("serialized form values:", $(event.target).serialize());
+    $.ajax({
+    url: "/tweets", 
+    data: $(event.target).serialize(), 
+    success: ()=> {
+      console.log("tweet successfully created!");
+    },
+    method: "POST"
+});
+  
+});
+
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#tweet-text').on("keyup", function(){
+    let tweet = $(this).val()
+    console.log("keypress event logged.");
+    let counterElement = $('#tweet-text').siblings("div").children(".counter")
+    counterElement.text(140 - tweet.length);
+    console.log(counterElement);
+    if (tweet.length >= 140) {
+      $("output").css({color: "red"});
+    } else {
+      $("output").css({color: "black"})
+    }
+  });
+  });
+  
