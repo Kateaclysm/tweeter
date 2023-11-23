553258abca37cc3f5371b86b6ac3ab487b8df7e8
Katelynn <grayspence2@hotmail.com>
2023-11-23 15:05:16 -0500

    Fixed layout bug where the header would stretch vertically when more tweets were added or an error message ocurred while in desktop format

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,23 +5,25 @@
  */
 
 
-$(document).ready(function(){
- // Hide the error message until needed 
+$(document).ready(function() {
+  // Hide the error message until needed 
   $('main.container > .error-container').hide();
 
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
   const createTweetElement = function(tweetData) {
-  // Create the HTML structure using `tweetData`
+    // Create the HTML structure using `tweetData`
 
-  /* VV formats the date to something human-readable!*/
-  const timeAgo = moment(tweetData.created_at).fromNow();
-  const $tweetText = $('<p>').text(tweetData.content.text);
-  const $tweet = $(`<article class="tweet">
+    /* VV formats the date to something human-readable!*/
+    const timeAgo = moment(tweetData.created_at).fromNow();
+    const $tweetText = $('<p>').text(tweetData.content.text);
+
+    /* VVV HTML Code for formatting the tweets from the initial tweets json*/
+    const $tweet = $(`<article class="tweet">
     <header>
       <img src="${tweetData.user.avatars}" alt="${tweetData.user.name}'s avatar"/>
       <h2>${tweetData.user.name}</h2>
@@ -39,20 +41,20 @@ $(document).ready(function(){
     </footer>
   </article>`);
 
-  return $tweet;
-};
+    return $tweet;
+  };
 
   const renderTweets = function(tweets) {
     // Empty the container first!
     $('.main-container').empty();
-  
+
     //Loop through the array of tweets!
     for (let tweet of tweets) {
       const $tweet = createTweetElement(tweet);
       $('.main-container').append($tweet);
     }
   };
-  
+  /* Error handling code as well as submission handling */
   $('#new-tweet-submission').on("submit", function(event) {
     event.preventDefault();
     let textLength = $('#tweet-text').val().length;
@@ -71,26 +73,25 @@ $(document).ready(function(){
     console.log("serialized form values:", $(event.target).serialize());
 
     $.ajax({
-    method: "POST",
-    url: "/tweets", 
-    data: $(event.target).serialize(), 
-    success: ()=> {
-      console.log("tweet successfully created!");
-      loadTweets();
+      method: "POST",
+      url: "/tweets",
+      data: $(event.target).serialize(),
+      success: () => {
+        loadTweets();
       },
     });
   });
 
-  const loadTweets = function () {
-      $.ajax({
-        method: "GET",
-        url: "/tweets", 
-        success: (parameter)=> {
-          console.log("tweet successfully created!", renderTweets(parameter));
-        },
+  const loadTweets = function() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      success: (parameter) => {
+        console.log("tweet successfully created!", renderTweets(parameter));
+      },
     });
   };
-  
+
   loadTweets();
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
-  $('#tweet-text').on("keyup", function(){
-    let tweet = $(this).val()
+  $('#tweet-text').on("keyup", function() {
+    let tweet = $(this).val();
     console.log("keypress event logged.");
-    let counterElement = $('#tweet-text').siblings("div").children(".counter")
+    let counterElement = $('#tweet-text').siblings("div").children(".counter");
     counterElement.text(140 - tweet.length);
     console.log(counterElement);
     if (tweet.length >= 140) {
-      $("output").css({color: "red"});
+      $("output").css({ color: "red" });
       // This slides down the error text if they go over 140 characters.
       $(".error-msg").text("You can't send a tweet containing over 140 characters!");
       $("main.container > .error-container").slideDown();
-    return;
+      return;
     } else {
       // Otherwise, if they are under the character limit, slide it up.
-      $("output").css({color: "black"})
+      $("output").css({ color: "black" });
       $("main.container > .error-container").slideUp();
     }
   });
-  });
-  
+});
