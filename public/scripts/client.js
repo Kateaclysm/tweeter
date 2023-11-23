1377a420d6972db8d6bfe6fc652ded197af79955
Katelynn <grayspence2@hotmail.com>
2023-11-22 20:59:17 -0500

    major layout changes using flexbox implmented; have header and tweet content side by side in desktop view

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,102 +5,92 @@
  */
 
 
+$(document).ready(function(){
+ // Hide the error message until needed 
+  $('main.container > .error-container').hide();
 
-const createTweetElement = function(tweetData) {
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const createTweetElement = function(tweetData) {
   // Create the HTML structure using `tweetData`
 
   /* VV formats the date to something human-readable!*/
   const timeAgo = moment(tweetData.created_at).fromNow();
-
+  const $tweetText = $('<p>').text(tweetData.content.text);
   const $tweet = $(`<article class="tweet">
     <header>
       <img src="${tweetData.user.avatars}" alt="${tweetData.user.name}'s avatar"/>
       <h2>${tweetData.user.name}</h2>
+      
       <span>${tweetData.user.handle}</span>
     </header>
-    <p>${tweetData.content.text}</p>
+    <p>${escape(tweetData.content.text)}</p>
     <footer>
       <time>${timeAgo}</time>
+      <div class="icons">
+      <i class="fa-solid fa-flag" id="footer-icon"></i>
+      <i class="fa-solid fa-retweet" id="footer-icon"> </i>
+      <i class="fa-solid fa-heart" id="footer-icon"> </i>
+      </div>
     </footer>
   </article>`);
 
   return $tweet;
 };
-$(document).ready(function(){
 
-  const tweetData =  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
+  const renderTweets = function(tweets) {
+    // Empty the container first!
+    $('.main-container').empty();
   
-  }
-
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-
-$('#tweets-container').append($tweet);
-})
-
-
-const renderTweets = function(tweets) {
-  // Empty the container first!
-  $('#tweets-container').empty();
-
-  //Loop through the array of tweets!
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
-  }
-};
-
-
-
-$(document).ready(function(){
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+    //Loop through the array of tweets!
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('.main-container').append($tweet);
     }
-  ];
-  renderTweets(data);
-
+  };
+  
   $('#new-tweet-submission').on("submit", function(event) {
     event.preventDefault();
+    let textLength = $('#tweet-text').val().length;
+    if (!$('#tweet-text').val()) {
+      event.preventDefault();
+      $("main.container > .error-container").slideDown();
+      $("h2.error-msg").text("You can't send a blank tweet!");
+    };
+    if (textLength > 140) {
+      event.preventDefault();
+      $(".error-msg").text("You can't send a tweet containing over 140 characters!");
+      return;
+    }
+    console.log(textLength);
     console.log("inside submit event handler", event.target);
     console.log("serialized form values:", $(event.target).serialize());
+
     $.ajax({
+    method: "POST",
     url: "/tweets", 
     data: $(event.target).serialize(), 
     success: ()=> {
       console.log("tweet successfully created!");
-    },
-    method: "POST"
-});
+      loadTweets();
+      },
+    });
+  });
+
+  const loadTweets = function () {
+      $.ajax({
+        method: "GET",
+        url: "/tweets", 
+        success: (parameter)=> {
+          console.log("tweet successfully created!", renderTweets(parameter));
+        },
+    });
+  };
   
-});
+  loadTweets();
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,8 +7,14 @@ $(document).ready(function() {
     console.log(counterElement);
     if (tweet.length >= 140) {
       $("output").css({color: "red"});
+      // This slides down the error text if they go over 140 characters.
+      $(".error-msg").text("You can't send a tweet containing over 140 characters!");
+      $("main.container > .error-container").slideDown();
+    return;
     } else {
+      // Otherwise, if they are under the character limit, slide it up.
       $("output").css({color: "black"})
+      $("main.container > .error-container").slideUp();
     }
   });
   });
